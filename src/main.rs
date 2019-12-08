8130306a2c99d23bf9dc88a4209c076eea43404f
6syy <win8_x@163.com>
2019-12-08 16:59:15 +0800

    使用rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/hello/<name>")]
+fn hello(name: String) -> String {
+    format!("Hello, {}!", name)
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index, hello]).launch();
 }
